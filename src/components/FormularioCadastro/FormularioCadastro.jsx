f6d7f662471b9f28ab3a44c316f2fc4b9bde8c81
GhaSilva <ghabriel-silva@outlook.com>
2021-09-30 19:34:03 -0300

    Fazendo validação simples

--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -1,10 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button, TextField, FormControlLabel, Switch } from "@material-ui/core";
 
 function FormularioCadastro() {
+  const [nome, setNome] = useState("");
+  const [sobrenome, setSobrenome] = useState("");
+  const [cpf, setCpf] = useState("");
   return (
-    <form>
+    <form
+      onSubmit={(event) => {
+        event.preventDefault();
+        console.log(nome, sobrenome, cpf);
+      }}
+    >
       <TextField
+        value={nome}
+        onChange={(event) => {
+          setNome(event.target.value);
+        }}
         id="nome"
         label="Nome"
         variant="outlined"
@@ -12,6 +24,10 @@ function FormularioCadastro() {
         margin="normal"
       />
       <TextField
+        value={sobrenome}
+        onChange={(event) => {
+          setSobrenome(event.target.value);
+        }}
         id="sobrenome"
         label="Sobrenome"
         variant="outlined"
@@ -19,6 +35,14 @@ function FormularioCadastro() {
         margin="normal"
       />
       <TextField
+        value={cpf}
+        onChange={(event) => {
+          let tmpCpf = event.target.value;
+          if (cpf.length >= 11) {
+            tmpCpf = tmpCpf.substr(0, 11);
+          }
+          setCpf(tmpCpf);
+        }}
         id="CPF"
         label="CPF"
         variant="outlined"
